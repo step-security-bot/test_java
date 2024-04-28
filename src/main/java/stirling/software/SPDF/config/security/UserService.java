5bf62332cb9c4adca6585fc27256dc16a954c569
Ludy87 <Ludy87@users.noreply.github.com>
2024-04-28 12:44:40 +0200

    Update UserService.java

--- src/main/java/stirling/software/SPDF/config/security/UserService.java
+++ src/main/java/stirling/software/SPDF/config/security/UserService.java
@@ -206,4 +206,9 @@ public class UserService implements UserServiceInterface {
     public boolean isUsernameValid(String username) {
         return username.matches("[a-zA-Z0-9]+");
     }
+
+    public void changeUserBlocked(User user, boolean b) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'changeUserBlocked'");
+    }
 }
