532dc6dc4a86dd0171a569773da7a00bc3f8898f
Ludy87 <Ludy87@users.noreply.github.com>
2024-05-09 09:10:18 +0200

    Update LoginAttemptService.java

--- src/main/java/stirling/software/SPDF/config/security/LoginAttemptService.java
+++ src/main/java/stirling/software/SPDF/config/security/LoginAttemptService.java
@@ -3,27 +3,17 @@ package stirling.software.SPDF.config.security;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import jakarta.annotation.PostConstruct;
 import stirling.software.SPDF.model.ApplicationProperties;
 import stirling.software.SPDF.model.AttemptCounter;
-import stirling.software.SPDF.model.User;
-import stirling.software.SPDF.repository.UserRepository;
 
 @Service
 public class LoginAttemptService {
 
     @Autowired ApplicationProperties applicationProperties;
-
-    private static final Logger logger = LoggerFactory.getLogger(LoginAttemptService.class);
-
-    @Autowired @Lazy private UserService userService;
-    @Autowired private UserRepository userRepository;
 
     private int MAX_ATTEMPTS;
     private long ATTEMPT_INCREMENT_TIME;
@@ -54,22 +44,6 @@ public class LoginAttemptService {
                         attemptCounter.increment();
                         return attemptCounter;
                     }
-                });
-        return attemptsCache.get(key).getAttemptCount() >= MAX_ATTEMPTS;
-    }
-
-    public boolean checkUserBlocked(String key) {
-        attemptsCache.compute(
-                key,
-                (k, attemptCounter) -> {
-                    if (attemptCounter == null
-                            || attemptCounter.shouldReset(ATTEMPT_INCREMENT_TIME)) {
-                        User user = userService.findByUsername(key).get();
-                        user.setIsUserBlocked(false);
-                        userRepository.save(user);
-                        return new AttemptCounter();
-                    }
-                    return attemptCounter;
                 });
         return attemptsCache.get(key).getAttemptCount() >= MAX_ATTEMPTS;
     }
