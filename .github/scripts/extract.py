cab9d9e9dd79072ab0dee392f6fe94bd200c9560
Ludy87 <Ludy87@users.noreply.github.com>
2024-07-12 00:12:31 +0200

    Update extract.py

--- .github/scripts/extract.py
+++ .github/scripts/extract.py
@@ -31,7 +31,9 @@ def get_issue(owner, repo, issue_number):
 
 def extract_version(text):
     # Anpassung des regulären Ausdrucks, um verschiedene Trennzeichen zu unterstützen und 1-3 stellige Zahlen zu erfassen
-    version_pattern = re.compile(r'### Version of Stirling-PDF\s*\n\s*([\d]{1,3}[\.\,\s][\d]{1,3}[\.\,\s][\d]{1,3})')
+    version_pattern = re.compile(
+        r"### Version of Stirling-PDF\s*\n\s*([\d]{1,3}[\.\,\s][\d]{1,3}[\.\,\s][\d]{1,3})"
+    )
     match = version_pattern.search(text)
     if match:
         return match.group(1).replace(" ", ".").replace(",", ".")
@@ -54,6 +56,13 @@ def add_label(owner, repo, issue_number, label):
     return response.json()
 
 
+def remove_label(owner, repo, issue_number, label):
+    url = f"https://api.github.com/repos/{owner}/{repo}/issues/{issue_number}/labels/{label}"
+    response = requests.delete(url, headers=headers, timeout=60)
+    response.raise_for_status()
+    return response.json()
+
+
 # Neueste Release-Version abrufen
 release_version = get_latest_release(REPO_OWNER, REPO_NAME)
 print(f"Neueste Release-Version: {release_version}")
@@ -69,7 +78,9 @@ if issue_version:
     print(f"Gefundene Version im Issue: {issue_version}")
     if version.parse(issue_version) < version.parse(release_version):
         add_label(REPO_OWNER, REPO_NAME, ISSUE_NUMBER, "out-of-date")
+    elif version.parse(issue_version) == version.parse(release_version):
+        remove_label(REPO_OWNER, REPO_NAME, ISSUE_NUMBER, "out-of-date")
 else:
     print("Keine Version im Issue gefunden.")
-    message = "Es wurde keine gültige Version von Stirling-PDF im angegebenen Format gefunden. Bitte geben Sie die Version im Format `### Version of Stirling-PDF\nx.y.z` an."
+    message = "Es wurde keine gültige Version von Stirling-PDF im angegebenen Format gefunden. Bitte geben Sie die Version im Format `0.0.2` an."
     post_comment(REPO_OWNER, REPO_NAME, ISSUE_NUMBER, message)
