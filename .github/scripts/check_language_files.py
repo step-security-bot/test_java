defd08f8d8a4fdad9af96a2daace6fa45529cc6c
Ludy87 <Ludy87@users.noreply.github.com>
2024-08-24 20:53:50 +0200

    test

--- .github/scripts/check_language_files.py
+++ .github/scripts/check_language_files.py
@@ -1,7 +1,4 @@
-"""check_language_files.py"""
-
 import os
-import glob
 import argparse
 
 
@@ -10,62 +7,55 @@ def read_properties(file_path):
         return file.read().splitlines()
 
 
-def check_defference(reference_file, file_list):
+def check_difference(reference_file, file_list):
     reference_list = read_properties(reference_file)
-    isDiff = False
+    is_diff = False
 
-    for file_paths in file_list:
-        print(file_paths[0])
-        for file_path in glob.glob(file_paths[0]):
-            if (
-                file_path == reference_file
-                or file_path.endswith("GB.properties")
-                or not file_path.endswith(".properties")
-                or not os.path.basename(file_path).startswith("messages_")
-            ):
-                continue
-            current_list = read_properties(file_path)
-            reference_list_len = len(reference_list)
-            current_list_len = len(current_list)
+    for file_path in file_list:
+        if (
+            file_path == reference_file
+            or not file_path.endswith(".properties")
+            or not os.path.basename(file_path).startswith("messages_")
+        ):
+            continue
 
-            if reference_list_len != current_list_len:
-                print(f"Differenz in der Datei: {os.path.basename(file_path)}")
-                isDiff = True
-            if reference_list_len > current_list_len:
-                print(
-                    f"Es fehlen Zeilen! Entweder fehlen Kommentare, leere Zeilen oder Übersetzungstrings! {reference_list_len}:{current_list_len}"
-                )
-            elif reference_list_len < current_list_len:
-                print(
-                    f"Es gibt zuviele Zeilen! Überprüfen sie deine Übersetzungs Dateien! {reference_list_len}:{current_list_len}"
-                )
-    if isDiff:
+        current_list = read_properties(file_path)
+        reference_list_len = len(reference_list)
+        current_list_len = len(current_list)
+
+        if reference_list_len != current_list_len:
+            print(f"Differenz in der Datei: {os.path.basename(file_path)}")
+            is_diff = True
+        if reference_list_len > current_list_len:
+            print(
+                f"Es fehlen Zeilen! Entweder fehlen Kommentare, leere Zeilen oder Übersetzungstrings! {reference_list_len}:{current_list_len}"
+            )
+        elif reference_list_len < current_list_len:
+            print(
+                f"Es gibt zuviele Zeilen! Überprüfen sie deine Übersetzungs Dateien! {reference_list_len}:{current_list_len}"
+            )
+
+    if is_diff:
         print("Check fail")
-        # print(reference_list)
+        print(reference_list)
         exit(1)
     print("Check success")
 
-def list_of_strings(arg):
-    return arg.split(',')
 
 if __name__ == "__main__":
-    directory = os.getcwd() + "/src/main/resources"
     parser = argparse.ArgumentParser(description="Find missing Keys")
     parser.add_argument(
         "--reference-file",
-        default=os.path.join(directory, "messages_en_GB.properties"),
-        help="Pfad zur Referenzdatei aus dem main-Branch.",
+        required=True,
+        help="Pfad zur Referenzdatei.",
     )
     parser.add_argument(
         "--files",
-        type=list_of_strings,
-        nargs='+',
+        type=str,
         required=True,
-        help="List of changed files separated by spaces",
+        help="Liste der geänderten Dateien, durch Leerzeichen getrennt.",
     )
     args = parser.parse_args()
 
-    # Split the files into a list
-    # file_list = args.files.split()
-
-    check_defference(args.reference_file, args.files)
+    file_list = args.files.split()
+    check_difference(args.reference_file, file_list)
