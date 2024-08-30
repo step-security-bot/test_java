e3a1feafec3b4614f2930156edf1ed5829ce0fc7
Ludy87 <Ludy87@users.noreply.github.com>
2024-08-30 18:27:00 +0200

    Update check_language_files.py

--- .github/scripts/check_language_files.py
+++ .github/scripts/check_language_files.py
@@ -224,5 +224,5 @@ if __name__ == "__main__":
     file_list = args.files
     print(file_list)
     if file_list is None:
-        file_list = glob.glob(os.getcwd() + "/src/**/messages_*.properties", recursive=True)
+        file_list = glob.glob("src/**/messages_*.properties", recursive=True)
     check_difference(args.reference_file, file_list, args.branch)
