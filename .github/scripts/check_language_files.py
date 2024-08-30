dea47c195859fd0404e0f7aea35774d17fd3339b
Ludy87 <Ludy87@users.noreply.github.com>
2024-08-30 19:19:10 +0200

    Update check_language_files.py

--- .github/scripts/check_language_files.py
+++ .github/scripts/check_language_files.py
@@ -75,18 +75,18 @@ def write_json_file(file_path, updated_current_json):
                 f.write(f"{entry['key']}={entry['value']}\n")
 
 
-def push_difference_keys(reference_file, file_list, branch=""):
+def push_difference_keys(reference_file, file_list):
     reference_json = parse_properties_file(reference_file)
     for file_path in file_list:
-        basename_current_file = os.path.basename(branch + file_path)
+        basename_current_file = os.path.basename(file_path)
         if (
-            branch + file_path == reference_file
+            file_path == reference_file
             or not file_path.endswith(".properties")
             or not basename_current_file.startswith("messages_")
         ):
             continue
 
-        current_json = parse_properties_file(branch + file_path)
+        current_json = parse_properties_file(file_path)
         ref_json = []
         for reference in reference_json:
             for current in current_json:
@@ -96,38 +96,33 @@ def push_difference_keys(reference_file, file_list, branch=""):
                     if reference["key"] == current["key"]:
                         reference["value"] = current["value"]
             ref_json.append(reference)
-        print(f"BRANCH {branch}")
-        print(f"reference_file {reference_file}")
-        print(f"basename_current_file {basename_current_file}")
-        print(f"file_path {file_path}")
-        print(f"branch + file_path {branch + file_path}")
-        write_json_file(branch + file_path, ref_json)
+        write_json_file(file_path, ref_json)
 
 
 def check_difference_keys(reference_file, file_list, branch):
-    push_difference_keys(reference_file, file_list, branch + "/")
-    # reference_json = parse_properties_file(reference_file)
+    # push_difference_keys(reference_file, file_list, branch + "/")
+    reference_json = parse_properties_file(reference_file)
 
-    # for file_path in file_list:
-    #     basename_current_file = os.path.basename(branch + "/" + file_path)
-    #     if (
-    #         branch + "/" + file_path == reference_file
-    #         or not file_path.endswith(".properties")
-    #         or not basename_current_file.startswith("messages_")
-    #     ):
-    #         continue
+    for file_path in file_list:
+        basename_current_file = os.path.basename(branch + "/" + file_path)
+        if (
+            branch + "/" + file_path == reference_file
+            or not file_path.endswith(".properties")
+            or not basename_current_file.startswith("messages_")
+        ):
+            continue
 
-    #     current_json = parse_properties_file(branch + "/" + file_path)
-    #     ref_json = []
-    #     for reference in reference_json:
-    #         for current in current_json:
-    #             if current["type"] == "entry":
-    #                 if reference["type"] != "entry":
-    #                     continue
-    #                 if reference["key"] == current["key"]:
-    #                     reference["value"] = current["value"]
-    #         ref_json.append(reference)
-    #     write_json_file(branch + "/" + file_path, ref_json)
+        current_json = parse_properties_file(branch + "/" + file_path)
+        ref_json = []
+        for reference in reference_json:
+            for current in current_json:
+                if current["type"] == "entry":
+                    if reference["type"] != "entry":
+                        continue
+                    if reference["key"] == current["key"]:
+                        reference["value"] = current["value"]
+            ref_json.append(reference)
+        write_json_file(branch + "/" + file_path, ref_json)
 
 
 def read_properties(file_path):
