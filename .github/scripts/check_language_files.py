951ad0a415c85fa68cb5eaca7604056e4f768dbd
Ludy87 <Ludy87@users.noreply.github.com>
2024-08-31 09:28:37 +0200

    Update check_language_files.py

--- .github/scripts/check_language_files.py
+++ .github/scripts/check_language_files.py
@@ -167,7 +167,7 @@ def check_for_differences(reference_file, file_list, branch):
             report.append("")
             report.append(f"#### ***{basename_current_file}*** will be corrected...")
             report.append("")
-            update_missing_keys(reference_file, [file_path], branch)
+            update_missing_keys(reference_file, [file_path], branch + "/")
         else:
             report.append("- ✅ Test 1 passed")
 
