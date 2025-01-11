0b97d2d429f32625b8bd376510b2d4ac32d6fdc7
Ludy87 <Ludy87@users.noreply.github.com>
2025-01-11 13:13:11 +0100

    docker

--- test.sh
+++ test.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# Function to check the health of the service with a timeout of 60 seconds
+# Function to check the health of the service with a timeout of 80 seconds
 check_health() {
     local service_name=$1
     local compose_file=$2
     local end=$((SECONDS+60))
 
     echo -n "Waiting for $service_name to become healthy..."
-    until [ "$(docker inspect --format='{{json .State.Health.Status}}' "$service_name")" == '"healthy"' ] || [ $SECONDS -ge $end ]; do
-        sleep 3
-        echo -n "."
-        if [ $SECONDS -ge $end ]; then
-            echo -e "\n$service_name health check timed out after 60 seconds."
-            echo "Printing logs for $service_name:"
+	until [ "$(docker inspect --format='{{json .State.Health.Status}}' "$service_name")" == '"healthy"' ] || [ $SECONDS -ge $end ]; do
+		sleep 3
+		echo -n "."
+		if [ $SECONDS -ge $end ]; then
+			echo -e "\n$service_name health check timed out after 80 seconds."
+			echo "Printing logs for $service_name:"
             docker logs "$service_name"
-            return 1
-        fi
-    done
-    echo -e "\n$service_name is healthy!"
-    echo "Printing logs for $service_name:"
+			return 1
+		fi
+	done
+	echo -e "\n$service_name is healthy!"
+	echo "Printing logs for $service_name:"
     docker logs "$service_name"
     return 0
 }
@@ -62,7 +62,7 @@ run_tests() {
 
 # Main testing routine
 main() {
-    SECONDS=0
+	SECONDS=0
 
     export DOCKER_ENABLE_SECURITY=false
     # Run the gradlew build command and check if it fails
@@ -73,28 +73,16 @@ main() {
 
 
     # Building Docker images
-    docker build --no-cache --build-arg VERSION_TAG=alpha -t ludy87/s-pdf:latest -f ./Dockerfile .
-    docker build --no-cache --build-arg VERSION_TAG=alpha -t ludy87/s-pdf:latest-ultra-lite -f ./Dockerfile-ultra-lite .
+    # docker build --no-cache --pull --build-arg VERSION_TAG=alpha -t ludy87/s-pdf:latest -f ./Dockerfile .
+    docker build --no-cache --pull --build-arg VERSION_TAG=alpha -t ludy87/s-pdf:latest-ultra-lite -f ./Dockerfile.ultra-lite .
 
     # Test each configuration
     run_tests "Stirling-PDF-Ultra-Lite" "./exampleYmlFiles/docker-compose-latest-ultra-lite.yml"
-    docker-compose -f "./exampleYmlFiles/docker-compose-latest-ultra-lite.yml" down
+	docker-compose -f "./exampleYmlFiles/docker-compose-latest-ultra-lite.yml" down
 
 
-    run_tests "Stirling-PDF" "./exampleYmlFiles/docker-compose-latest.yml"
-    if [ $? -eq 0 ]; then
-        cd cucumber
-        if behave; then
-            passed_tests+=("Stirling-PDF-Regression")
-        else
-            failed_tests+=("Stirling-PDF-Regression")
-            echo "Printing docker logs of failed regression"
-            docker logs "Stirling-PDF"
-            echo "Printed docker logs of failed regression"
-        fi
-        cd ..
-    fi
-    docker-compose -f "./exampleYmlFiles/docker-compose-latest.yml" down
+    #run_tests "Stirling-PDF" "./exampleYmlFiles/docker-compose-latest.yml"
+	#docker-compose -f "./exampleYmlFiles/docker-compose-latest.yml" down
 
     export DOCKER_ENABLE_SECURITY=true
     # Run the gradlew build command and check if it fails
@@ -105,34 +93,46 @@ main() {
 
 
     # Building Docker images with security enabled
-    docker build --no-cache --build-arg VERSION_TAG=alpha -t ludy87/s-pdf:latest -f ./Dockerfile .
-    docker build --no-cache --build-arg VERSION_TAG=alpha -t ludy87/s-pdf:latest-ultra-lite -f ./Dockerfile-ultra-lite .
-    docker build --no-cache --build-arg VERSION_TAG=alpha -t ludy87/s-pdf:latest-fat -f ./Dockerfile-fat .
+   # docker build --no-cache --pull --build-arg VERSION_TAG=alpha -t stirlingtools/stirling-pdf:latest -f ./Dockerfile .
+ #   docker build --no-cache --pull --build-arg VERSION_TAG=alpha -t stirlingtools/stirling-pdf:latest-ultra-lite -f ./Dockerfile.ultra-lite .
+    docker build --no-cache --pull --build-arg VERSION_TAG=alpha -t stirlingtools/stirling-pdf:latest-fat -f ./Dockerfile.fat .
 
 
     # Test each configuration with security
-    run_tests "Stirling-PDF-Ultra-Lite-Security" "./exampleYmlFiles/docker-compose-latest-ultra-lite-security.yml"
-    docker-compose -f "./exampleYmlFiles/docker-compose-latest-ultra-lite-security.yml" down
-    run_tests "Stirling-PDF-Security" "./exampleYmlFiles/docker-compose-latest-security.yml"
-    docker-compose -f "./exampleYmlFiles/docker-compose-latest-security.yml" down
+  #  run_tests "Stirling-PDF-Ultra-Lite-Security" "./exampleYmlFiles/docker-compose-latest-ultra-lite-security.yml"
+	#docker-compose -f "./exampleYmlFiles/docker-compose-latest-ultra-lite-security.yml" down
+  #  run_tests "Stirling-PDF-Security" "./exampleYmlFiles/docker-compose-latest-security.yml"
+#	docker-compose -f "./exampleYmlFiles/docker-compose-latest-security.yml" down
 
-    run_tests "Stirling-PDF-Security-Fat" "./exampleYmlFiles/docker-compose-latest-fat-security.yml"
-    docker-compose -f "./exampleYmlFiles/docker-compose-latest-fat-security.yml" down
+	run_tests "Stirling-PDF-Security-Fat" "./exampleYmlFiles/test_cicd.yml"
+	if [ $? -eq 0 ]; then
+		cd cucumber
+		if python -m behave; then
+			passed_tests+=("Stirling-PDF-Regression")
+		else
+			failed_tests+=("Stirling-PDF-Regression")
+			echo "Printing docker logs of failed regression"
+			docker logs "Stirling-PDF-Security-Fat"
+			echo "Printed docker logs of failed regression"
+		fi
+		cd ..
+	fi
+	docker-compose -f "./exampleYmlFiles/docker-compose-latest-fat-security.yml" down
 
     # Report results
     echo "All tests completed in $SECONDS seconds."
 
 
-    if [ ${#passed_tests[@]} -ne 0 ]; then
-        echo "Passed tests:"
-    fi
+	if [ ${#passed_tests[@]} -ne 0 ]; then
+		echo "Passed tests:"
+	fi
     for test in "${passed_tests[@]}"; do
         echo -e "\e[32m$test\e[0m"  # Green color for passed tests
     done
 
-    if [ ${#failed_tests[@]} -ne 0 ]; then
-        echo "Failed tests:"
-    fi
+	if [ ${#failed_tests[@]} -ne 0 ]; then
+		echo "Failed tests:"
+	fi
     for test in "${failed_tests[@]}"; do
         echo -e "\e[31m$test\e[0m"  # Red color for failed tests
     done
